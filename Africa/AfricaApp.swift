2412a37b401d4d1ad9ffca4d2f09bfdfeec2ef28
Muhammad Usman <m.usman@tdsoftware.de>
2021-09-14 16:30:29 +0500

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Muhammad Usman on 14/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
